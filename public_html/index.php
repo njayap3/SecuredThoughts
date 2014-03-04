75eca6d124f9601a096ef54d397d882415ba80ad
Neha Jayaprakash <jneha.aqua16@gmail.com>
2014-03-04 11:34:26 -0600

    About page done.

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,10 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
-		<link href='http://fonts.googleapis.com/css?family=Allura|Life+Savers|Gochi+Hand|Gafata|Oregano|Satisfy' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" type="text/css" href="css/robot.css">
-		<link href='https://fonts.googleapis.com/css?family=Poller+One' rel='stylesheet' type='text/css'>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-		<style type="text/css">
+<!-- includes the css files -->
+<?php include 'header.php'; ?>
+<body>
+	<?php include 'menubar.php'; ?>
+		<style type="text/css" scope>
 		#alertbanner {
 			position: fixed;
 			width: 500px;
@@ -19,29 +16,7 @@
 			background: #C0C0C0;
 			text-align: center;
 		}
-
-
 		</style>
-	</head>
-	<body>
-		<!-- ********HEADER*********** -->
-		<nav class="navbar navbar-inverse" role="navigation">
-  		<div class="container-fluid">
-    		<div class="navbar-header">
-      		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-       			<span class="sr-only">Toggle navigation</span>
-        		<span class="icon-bar"></span>
-        		<span class="icon-bar"></span>
-        		<span class="icon-bar"></span>
-      		</button>
-      		<a class="navbar-brand" href="#" style="font-family: 'Life Savers',cursive; font-size:40px;">Secured Thoughts</a>
-    		</div>
-      		<ul class="nav navbar-nav navbar-right">
-        		<li><a href="#" style="font-family: 'Life Savers',cursive; font-size:20px;">About Me</a></li>
-      		</ul>
-  		</div>
-		</nav>
-		<!-- ******************** -->
 		<div id="badguy1" class="robot pull-left">
   			<div class="robo-beep"></div>
   			<div class="robo-brain laser"></div>
@@ -70,14 +45,14 @@
   			</div>
   			<div class="robo-foot"></div>
 		</div>
-
+		<!-- includes jss files -->
+		<?php include 'footer.php'; ?>
 	<script type="text/javascript">
 		jQuery('#badguy1').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e){
 			jQuery('#alertbanner').removeClass('hide');
 
 		});
 
-
 	</script>
-	</body>
+</body>
 </html>
